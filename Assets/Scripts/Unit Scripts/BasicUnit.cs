66b0576c43adaeeb0550eda91bf5173f474dbe30
Hugh <hugh.hannan@student.uts.edu.au>
2022-08-04 16:58:28 +1000

    Updated units to change colour based on team

--- Assets/Scripts/Unit Scripts/BasicUnit.cs
+++ Assets/Scripts/Unit Scripts/BasicUnit.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class BasicUnit : MonoBehaviour
 {
     public UnitInfo unitInfo;
-    
+    public Material lightMaterial;
+    public Material darkMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
+        SetTeamColour(unitInfo.team);
+        
+        //Setting position on game board based on team
         UnitMovement.StartMovement(unitInfo);
         transform.position = Hex.GetXYZPosition(unitInfo.Q, unitInfo.R);
     }
@@ -16,8 +21,26 @@ public class BasicUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movement commands
         UnitMovement.UpdateMovement(unitInfo);
         transform.position = Hex.GetXYZPosition(unitInfo.Q, unitInfo.R);
+    }
+
+    private void SetTeamColour (int team)
+    {
+        if (team == 1)
+        {
+            GetComponent<MeshRenderer>().material = lightMaterial;
+        }        
+        
+        if (team == 2)
+        {
+            GetComponent<MeshRenderer>().material = darkMaterial;
+        }
+        else
+        {
+            Debug.Log("team was not selected");
+        }
     }
 }
 
